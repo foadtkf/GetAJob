83048aa5e673de3521b5a3ef802815eae1a42e87
Fakhrul Islam <fakhrulif@gmail.com>
2023-04-09 17:40:19 +0600

    User Signup with firebase authentication Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   public/icon.png 	modified:   public/index.html 	modified:   src/App.js 	modified:   src/features/auth/authSlice.js 	new file:   src/firebase/firebase.config.js 	modified:   src/layout/main/Navbar.js 	modified:   src/pages/Signup.js 	modified:   yarn.lock

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,13 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
 const initialState = {
   email: "",
   role: "",
   isLoading: true,
   isError: false,
-  errorMessage: "",
+  error: "",
 };
+export const createUser = createAsyncThunk(
+  "auth/createUser",
+  async ({ email, password }, thunkApi) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    return data;
+  }
+);
 const authSlice = createSlice({
   name: "auth",
   initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state, payload) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.email = action.payload;
+        state.error = "";
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.error.message;
+      });
+  },
 });
 export default authSlice.reducer;
--- src/layout/main/Navbar.js
+++ src/layout/main/Navbar.js
@@ -9,12 +9,13 @@ const Navbar = () => {
   return (
     <nav
       className={`h-14 fixed w-full z-[999] ${
-        pathname === "/" ? null : "bg-white"
+        // pathname === "/" ? null : "bg-white"
+        pathname === "/" ? "bg-white" : "bg-white"
       }`}
     >
       <ul className="max-w-7xl mx-auto flex gap-3 h-full items-center">
         <li className="flex-auto font-semibold text-2xl">
-          <Link to="/">GetAJob</Link>
+          <Link to="/">GetAJob!</Link>
         </li>
         <li>
           <Link className="hover:text-primary" to="/jobs">
